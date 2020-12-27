63ca6280d154c2383abd3b686f04b90261495898
Dragon11a22 <76639004+Dragon11a22@users.noreply.github.com>
2020-12-27 06:46:10 +0200

    Add files via upload

---
+++ Dragon_semple.sh
@@ -0,0 +1,45 @@
+echo "Dragon loading ........."
+apt update -y
+apt upgrade -y
+pkg install update -y
+pkg install upgrade -y
+termux-setup-storage
+apt-get update -y
+apt-get upgrade -y
+apt install php -y
+apt install python -y
+apt install python2 -y
+apt install git -y
+apt install golang -y
+apt install host -y
+apt install nano -y
+apt install havij -y
+apt install hydra -y
+apt install wireshark -y
+apt install cmatrix -y
+pkg install figlet -y
+pkg install wget -y
+pkg install wget -y
+pkg install python2 -y
+pkg install python2-dev -y
+apt install wireshark -y
+pkg install cowsay -y
+pkg install toilet -y
+pkg install ruby -y
+pkg install help -y
+gem install lolcat -y
+pkg install curl -y
+pkg install wgetrc -y
+pkg install unzip -y
+pkg install openssh -y
+pkg install tor -y
+pkg install uzip -y
+pkg install net-tools -y
+pkg install unrar -y
+pkg install clang -y
+pkg install w3m -y
+pkg install proot -y
+pip2 install wget -y
+pip2 install requests -y
+apt update && apt upgrade -y
+clear
